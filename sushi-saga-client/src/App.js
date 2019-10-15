2155fa5bc9c139fce7c8119aa55d496d60245a48
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-10-15 17:11:38 +0100

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -6,12 +6,51 @@ import Table from './containers/Table';
 const API = "http://localhost:3000/sushis"
 
 class App extends Component {
+  constructor() {
+    super()
+      this.state={
+        sushis: [],
+        moneyLeft: 100,
+        index: 0, 
+        plates: []
+    }
+  }
+
+  get = (url) => {
+    return fetch(url).then(response => response.json())
+  }
+
+  componentDidMount() {
+    this.get(API).then(sushisList => this.setState({sushis: sushisList}))
+  }
+
+  // always just set the index to itself, plus 4 (only want 4 sushis at a time)
+  increaseIndex = () => this.setState({ index: this.state.index + 4 })
+
+  // the selection is (start) the sushis index, plus (end) the next 4 along
+  getSelection = () => {
+    return this.state.sushis.slice(this.state.index, this.state.index + 4)
+  }
+
+
+  handleEaten = (yum) => {
+    if (yum.price > this.state.moneyLeft) {
+      alert("You don't have enough money for that sushi")
+    } else {
+        this.setState({
+        plates: [...this.state.plates, yum],
+        moneyLeft: this.state.moneyLeft-yum.price,
+        index: this.state.index
+      })
+    }
+  }
 
   render() {
+    const renderSushis = this.getSelection()
     return (
       <div className="app">
-        <SushiContainer  />
-        <Table />
+        <SushiContainer sushis={renderSushis} plates={this.state.plates} increaseIndex={this.increaseIndex} handleEaten={this.handleEaten} />
+        <Table moneyLeft={this.state.moneyLeft} plates={this.state.plates}/>
       </div>
     );
   }
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,16 +1,13 @@
 import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
+import Sushi from '../components/Sushi'
 
 const SushiContainer = (props) => {
   return (
     <Fragment>
       <div className="belt">
-        {
-          /* 
-             Render Sushi components here!
-          */
-        }
-        <MoreButton />
+        {props.sushis.map((sushi, i) => <Sushi key={sushi.id} sushi={sushi} plates={props.plates} handleEaten={()=>props.handleEaten(sushi)} />)}
+        <MoreButton getMoreSushi={props.increaseIndex}/>
       </div>
     </Fragment>
   )
